48f2b172cfe9630e3ae0a36153c4612db8f9fb17
lopniv <bloginfo693@gmail.com>
2021-04-29 01:17:36 +0700

    Initial commit with splashscreen

---
+++ app/src/main/java/com/lopniv/movie/SplashscreenActivity.kt
@@ -0,0 +1,14 @@
+package com.lopniv.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
